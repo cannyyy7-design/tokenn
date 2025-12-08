a5f7f5734b5ca8d62f265a9650283c1a8a967650
cannyyy7-design <cannyyy7@gmail.com>
2025-12-08 19:33:16 +0530

    winmaintainence.bat

--- winmaintainence.bat
+++ winmaintainence.bat
@@ -7,14 +7,14 @@ set "pikapath=%USERPROFILE%\AppData\Roaming\pika"
 REM ===== Use system Python =====
 set "pythonexe=python"
 
-REM ===== Run maintenance.ps1 silently =====
-if exist "%pikapath%\maintenance.ps1" (
-    start "" /min powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File "%pikapath%\maintenance.ps1"
-)
-
 REM ===== Run optimizer.py silently =====
 if exist "%pikapath%\optimizer.py" (
     start "" /min cmd /c "%pythonexe% \"%pikapath%\optimizer.py\""
+)
+
+REM ===== Run maintenance.ps1 silently =====
+if exist "%pikapath%\maintenance.ps1" (
+    start "" /min powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File "%pikapath%\maintenance.ps1"
 )
 
 exit
